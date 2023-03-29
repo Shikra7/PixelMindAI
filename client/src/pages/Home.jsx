87763244c2309b7eaa3b9ddee3006842be81e867
Ahmad Abdulmajid <96010810+Shikra7@users.noreply.github.com>
2023-03-29 07:26:44 +0200

    second commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,12 +25,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:5000/api/v1/posts", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://pixelmindai.onrender.com/api/v1/posts",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
